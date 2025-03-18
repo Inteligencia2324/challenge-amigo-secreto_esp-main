1bc8f8327e1c2e25edee2fffeb17433f26d5224c
Inteligencia2324 <analista10071@gmail.com>
2025-03-17 21:55:08 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//ALmacenar nombres de los amigos ingresados
+
 let amigos = [];
 
 /**
